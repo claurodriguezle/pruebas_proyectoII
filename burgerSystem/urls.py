e7c07f944f695b8e8e96023b38011f01a6cce6c8
Janyne Ruiz <ruizjanyne@gmail.com>
2025-06-03 09:42:06 -0300

    Crea la app de usuarios y sus funcionalidades

--- burgerSystem/urls.py
+++ burgerSystem/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('administrador.urls')),
     path('pedidos/', include('pedidos.urls')),
+    path('usuarios/', include('usuarios.urls')),
 ]
 
 if settings.DEBUG:
